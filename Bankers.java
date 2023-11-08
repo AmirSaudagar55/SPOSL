cfea8184500e5d6d3483b943c9cb87224db16b55
NMA007 <121443390+NMA007@users.noreply.github.com>
2023-11-08 07:15:50 +0530

    Example Added

--- Bankers.java
+++ Bankers.java
@@ -77,3 +77,19 @@ public class Bankers {
         b.isSafe();
     }
 }
+
+
+
+
+
+
+
+// Process     Allocation  Max     Available   Need
+//             A B C      A B C    A B C       A B C
+// 0           0 1 0      7 5 3    3 3 2       7 4 3
+// 1           2 0 0      3 2 2                1 2 2
+// 2           3 0 2      9 0 2                6 0 0
+// 3           2 1 1      2 2 2                0 1 1
+// 4           0 0 2      4 3 3                4 3 1
+
+// Safe Sequence : 1  3 4 0 2
